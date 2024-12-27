5c94de65907ba033420abb93f89fe6cd0948a31f
devesh <devesh@aggarwal.com>
2024-12-26 16:32:09 -0800

    add magnitude calculation and increase efficiency
    adds magnitude calculation as -2.5*log10(flux)
    
    increases computational efficiency by loosening restrictions on the stellar detection algorithm and instead keeping only the brightest star (Polaris) detected

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from photutils.detection import DAOStarFinder
 
 directory = 'images/night2'
 flux_values = []
+mag_values = []
 
 # sort directory in alphabetical order
 file_list = sorted(os.listdir(directory))
@@ -24,11 +25,13 @@ for filename in file_list:
     mean, median, std = sigma_clipped_stats(data, sigma=3.0)
     #print(np.array((mean, median, std)))
 
-    # # find stars
-    daofind = DAOStarFinder(fwhm=30.0, threshold=400.*std)
+    # find stars
+    daofind = DAOStarFinder(fwhm=5.0, threshold=5.*std, brightest=1) # keeps only the brightest star
     sources = daofind(data - median)
+    #sources.sort('flux', reverse=True)
     print(f'filename {filename} with flux {sources['flux'][0]}')
     flux_values.append(sources['flux'][0])
+    mag_values.append(sources['mag'][0]) # calculated as -2.5 * log10(flux)
 
 # UNCOMMENT TO VISUALIZE AND DEBUG INDIVIDAUL FILES
 # filepath = os.path.join('images/night2/Light_Polaris_10.0s_IRCUT_20241127-202252.fit')
@@ -53,9 +56,32 @@ for filename in file_list:
 # apertures.plot(color='blue', lw=1.5, alpha=0.5)
 # plt.show()
 
-# visualize the flux values
-plt.plot(flux_values)
+# visualize the flux and magnitude values
+plt.figure(figsize=(10, 5))
+
+# Plot flux values
+plt.subplot(1, 2, 1)
+plt.plot(flux_values, label='Flux values')
 plt.xlabel('File index')
 plt.ylabel('Flux value')
 plt.title('Flux values of detected stars')
+# Add a trendline for flux values
+z_flux = np.polyfit(range(len(flux_values)), flux_values, 2)
+p_flux = np.poly1d(z_flux)
+plt.plot(range(len(flux_values)), p_flux(range(len(flux_values))), "r--", label='Trendline')
+plt.legend()
+
+# Plot magnitude values
+plt.subplot(1, 2, 2)
+plt.plot(mag_values, label='Magnitude values', color='orange')
+plt.xlabel('File index')
+plt.ylabel('Magnitude value')
+plt.title('Magnitude values of detected stars')
+# Add a trendline for magnitude values
+z_mag = np.polyfit(range(len(mag_values)), mag_values, 2)
+p_mag = np.poly1d(z_mag)
+plt.plot(range(len(mag_values)), p_mag(range(len(mag_values))), "r--", label='Trendline')
+plt.legend()
+
+plt.tight_layout()
 plt.show()
